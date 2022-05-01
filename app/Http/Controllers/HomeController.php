dea6d920657d6513f307eef4a091ae5f7fc07b4e
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-02 07:10:21 +0900

    データベースからメモを取得

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('create');
+        // このタイミングでメモを取得
+        $memos = Memo::select('memos.*')
+        ->Where('user_id','=',\Auth::id())
+        ->WhereNull('deleted_at')
+        ->orderBy('updated_at','DESC')
+        ->get();
+
+        return view('create',compact('memos'));
+    }
+
+    public function store(Request $request)
+    {
+        // $request変数の中にあるものをすべて$postsの中に入れる
+        $posts=$request->all();
+
+        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+        return redirect(route('home'));
     }
 }
