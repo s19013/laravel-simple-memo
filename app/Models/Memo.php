aa8c931767835f79a27ee9053db3ea7528b409e4
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-04 12:43:28 +0900

    リファクタリング

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Memo extends Model
 {
     use HasFactory;
+
+    public function getMymemo()
+    {
+        //クエリパラメータtagがあればタグで絞り込む
+        //なければすべて取得
+        $query_tag =\Request::query('tag');
+        // 条件に応じてクエリビルダーを追加
+        //クエリビルダーは分割できる
+
+        $query = Memo::query()->select('memos.*')
+        ->Where('user_id','=',\Auth::id())
+        ->WhereNull('deleted_at')
+        ->orderBy('updated_at','DESC');
+
+        if (!empty($query_tag)) {
+            // ここで更にクエリを追加
+            $query->leftJoin('memo_tags','memo_tags.memo_id','=','memos.id')
+            ->where('memo_tags.tag_id','=',$query_tag);
+        }
+
+        $memos = $query->get();
+        return $memos;
+    }
 }
